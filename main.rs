d41b3c7d841baa2dfb84fd6569d1963d28d0107f
JeanChristophe <119110069+JeanChristopheMoon@users.noreply.github.com>
2023-12-22 23:35:39 +0100

    Create main.rs
    Main structure for creating Function (fn) and Context (struct).

---
+++ main.rs
@@ -0,0 +1,27 @@
+mod example1 {
+
+
+    pub fn intilizing(ctx: Context<buyingtickect>, ticket_price=u64,){
+
+
+
+    }
+
+
+
+
+
+
+}
+
+//////Context
+#[derive(Account)]
+pub struct buyingtickect<'info>{
+
+
+
+
+
+    
+}
+
